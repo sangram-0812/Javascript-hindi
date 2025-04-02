538a81b4656b007c81b5e178567ddcb520c381ee
sangram-0812 <rmgunjal72@gmail.com>
2025-04-02 10:09:01 +0000

    Variables information

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all javascript code as newer version
+
+// alert(3+3) // we are using node.js not browser, in browser it will show 6
+
+let name = "Sangram"
+let age = 24
+let isLoggedIn = true
+
+//Some more data types below ->
+
+//number - range is till 2 to the power 53
+
+//bigint
+
+//string = " "
+//boolean = true/false
+// null 
+// undefined
+
+/* here null has object data type and undefined has undefined data type */
+
+console.log("Sangram")
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
